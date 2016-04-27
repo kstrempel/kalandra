e06308d979abc785de074199637019f902135a85
kstrempel <kstrempel@gmail.com>
2016-04-27 13:33:35 +0200

    adds easiest mutex locking for connection manager

--- connections.go
+++ connections.go
@@ -2,23 +2,28 @@ package main
 
 import (
 	"github.com/gocql/gocql"
+	"sync"
 )
 
-type CachedSession struct {
-	session *gocql.Session
-}
-
-var sessions *CachedSession
+var sessions map[string]*gocql.Session
+var mutex sync.Mutex
 
 // GetSession returns a open session from a open connection using the
 // singleton pattern to reuses open sessions
 func GetSession(keyspace string) *gocql.Session {
-	// #TODO improve this for multiply keyspaces
-	if sessions == nil {
+	mutex.Lock()
+	defer mutex.Unlock()
+	session, ok := sessions[keyspace]
+	if ok == false {
+
+		if sessions == nil {
+			sessions = make(map[string]*gocql.Session)
+		}
 		cluster := gocql.NewCluster("127.0.0.1")
 		cluster.Keyspace = keyspace
 		session, _ := cluster.CreateSession()
-		sessions = &CachedSession{session: session}
+		sessions[keyspace] = session
+		return session
 	}
-	return sessions.session
+	return session
 }
