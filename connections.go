158531aaeb225fe833135a901e9964869ae64958
kstrempel <kstrempel@gmail.com>
2016-04-27 11:37:05 +0200

    adds a simple connection manager

--- connections.go
+++ connections.go
@@ -4,12 +4,21 @@ import (
 	"github.com/gocql/gocql"
 )
 
+type CachedSession struct {
+	session *gocql.Session
+}
+
+var sessions *CachedSession
+
 // GetSession returns a open session from a open connection using the
 // singleton pattern to reuses open sessions
 func GetSession(keyspace string) *gocql.Session {
-	cluster := gocql.NewCluster("127.0.0.1")
-	cluster.Keyspace = keyspace
-	session, _ := cluster.CreateSession()
-
-	return session
+	// #TODO improve this for multiply keyspaces
+	if sessions == nil {
+		cluster := gocql.NewCluster("127.0.0.1")
+		cluster.Keyspace = keyspace
+		session, _ := cluster.CreateSession()
+		sessions = &CachedSession{session: session}
+	}
+	return sessions.session
 }
